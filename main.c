1f370faa9813dc3e68f427ba5cfaeeca72e689fb
Xuan <105032571+Xuan901001@users.noreply.github.com>
2025-01-22 10:19:58 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include "gpio.h"
-#include <signal.h> // 信號處理
+#include <signal.h>
 #include <unistd.h>
 
 int main() {
-    signal(SIGINT, cleanup_gpio); // 註冊中斷信號處理
+    signal(SIGINT, cleanup_gpio); // 中斷信號ctrl+c
     setup_gpio();
-    set_gpio_mode(17, 0b001); // 設置 GPIO 17 為輸出模式
+    set_gpio_mode(17, 0b001); // 設GPIO 17輸出
 
     while (1) {
-        gpio_write(17, 1);  // 高電位
-        usleep(500000);     // 延遲 500ms
-        gpio_write(17, 0);  // 低電位
-        usleep(500000);     // 延遲 500ms
+        gpio_write(17, 1); 
+        usleep(500000);    
+        gpio_write(17, 0);  
+        usleep(500000);    
     }
 
     return 0;
